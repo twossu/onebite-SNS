53f962a5d849d0e9ad51eb0d6a64b628a7587176
twossu <nadao202@naver.com>
2025-11-02 19:50:31 +0900

    ~3.3

---
+++ onebite-exam/src/store/count.ts
@@ -0,0 +1,86 @@
+import { create } from "zustand";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+  devtools,
+} from "zustand/middleware";
+import { immer } from "zustand/middleware/immer";
+
+export const useCountStore = create(
+  devtools(
+    persist(
+      subscribeWithSelector(
+        immer(
+          combine({ count: 0 }, (set, get) => ({
+            actions: {
+              increaseOne: () => {
+                set((state) => {
+                  state.count += 1;
+                });
+              },
+              decreaseOne: () => {
+                set((state) => {
+                  state.count -= 1;
+                });
+              },
+            },
+          })),
+        ),
+      ),
+      {
+        name: "countStore",
+        partialize: (store) => ({
+          count: store.count,
+        }),
+        storage: createJSONStorage(() => sessionStorage),
+      },
+    ),
+    {
+      name: "countStore",
+    },
+  ),
+);
+
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Listner
+    console.log(count, prevCount);
+
+    const store = useCountStore.getState();
+    // useCountStore.setState((store)=>({ }))
+  },
+);
+
+// export const useCountStore = create<Store>((set, get) => ({
+//   count: 0,
+//   actions: {
+//     increaseOne: () => {
+//       set((store) => ({
+//         count: store.count + 1,
+//       }));
+//     },
+//     decreaseOne: () => {
+//       set((store) => ({
+//         count: store.count - 1,
+//       }));
+//     },
+//   },
+// }));
+
+export const useCount = () => {
+  const count = useCountStore((store) => store.count);
+  return count;
+};
+
+export const useIncreaseCount = () => {
+  const increase = useCountStore((store) => store.actions.increaseOne);
+  return increase;
+};
+
+export const useDecreaseCount = () => {
+  const decrease = useCountStore((store) => store.actions.decreaseOne);
+  return decrease;
+};
